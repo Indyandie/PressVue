4486783285ed15b1bb117bad12393d2a1719676a
majid_taheri <majid@taheri24.ir>
2019-04-07 15:41:59 +0430

    Update enhanceApp.js

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,7 +1,6 @@
 export default ({ router, siteData }) => {
-    const routesForNav = siteData.themeConfig.nav.filter(n=>n.link0).map(n => (
+    const routesForNav = siteData.themeConfig.nav.filter(n => n.link0).map(n => (
         { path: n.link, redirect: n.link0 }
     ));
     router.addRoutes(routesForNav);
-
 }
