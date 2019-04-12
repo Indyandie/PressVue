973094f7ad7cdb2f7c5702697ef626dcf69cfb06
Majid Taheri(I love optimization) <taheri24@users.noreply.github.com>
2019-04-12 11:14:08 +0430

    remove toggle button in mobile mode

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -3,41 +3,57 @@ import Vuex from 'vuex'
 import VueChartkick from 'vue-chartkick'
 import Chart from 'chart.js'
 import VueGoodTablePlugin from 'vue-good-table';
-export default ({Vue, router, siteData }) => {
+export default ({ Vue, router, siteData }) => {
     Vue.use(Vuex)
-    Vue.use(VueChartkick, {adapter: Chart})
-	Vue.use(VueGoodTablePlugin)
+    Vue.use(VueChartkick, { adapter: Chart })
+    Vue.use(VueGoodTablePlugin)
     const routesForNav = siteData.themeConfig.nav.filter(n => n.link0).map(n => (
         { path: n.link, redirect: n.link0 }
     ));
     router.addRoutes(routesForNav);
 }
-function processLinks(){
-    const links=document.querySelector('header.navbar .links');
-    if(!links) {
-        setTimeout(processLinks,200);
+function processLinks() {
+    const links = document.querySelector('header.navbar .links');
+    if (!links) {
+        setTimeout(processLinks, 200);
         return;
     }
-    const anchors=Array.from(  links.querySelectorAll('.nav-item a') ) ;
-    console.log(anchors);
-    for(const anchor of anchors){
-        const href=anchor.getAttribute('href');
-        if(href.includes('#toggle')){
+
+    const anchors = Array.from(links.querySelectorAll('.nav-item a'));
+    processSidebar();
+    for (const anchor of anchors) {
+        const href = anchor.getAttribute('href');
+        if (href.includes('#toggle')) {
             anchor.classList.add('toggle');
             anchor.parentElement.classList.add('toggle-nav');
-            anchor.setAttribute('href','#');
+            anchor.setAttribute('href', '#');
             const anchorClone = anchor.cloneNode(true);
 
             anchor.parentNode.replaceChild(anchorClone, anchor);
-anchorClone.addEventListener('click',toggleClick);
+            anchorClone.addEventListener('click', toggleClick);
         }
     }
     links.classList.add('ok')
-    
-} 
-function toggleClick(e){
 
-e.preventDefault();
-document.documentElement.classList.toggle('sidebar-hide');
+}
+
+function processSidebar() {
+    const links = document.querySelector('.theme-container>.sidebar>.nav-links');
+    if (!links) return;
+    const anchors = Array.from(links.querySelectorAll('.nav-item a'));
+
+    for (const anchor of anchors) {
+        const href = anchor.getAttribute('href');
+        if (href.includes('#toggle')) {
+            anchor.parentElement.style.display = 'none';
+
+        }
+    }
+
+}
+function toggleClick(e) {
+
+    e.preventDefault();
+    document.documentElement.classList.toggle('sidebar-hide');
 }
 processLinks();
