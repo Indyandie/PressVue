36e18cc6cb33ba6440909b66af3383cc4a4e11d3
majid_taheri <majid@taheri24.ir>
2019-04-20 15:27:30 +0430

    fix bugs

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -26,28 +26,55 @@ var simulateClick = function (elem) {
 function processSidebarButton() {
     const sidebarButton = document.querySelector('.sidebar-button');
     if (sidebarButton) {
-        sidebarButton.addEventListener('click', () => document.documentElement.classList.toggle('is-sidebar-open'));
-        document.body.clientWidth > 959 && simulateClick(sidebarButton);
+        sidebarButton.addEventListener('click', () => {
+
+            if (!document.querySelector('.theme-container.no-sidebar')) {
+                const { documentElement } = document;
+                documentElement.classList.toggle('is-sidebar-open');
+                localStorage.setItem('sidebar', documentElement.classList.contains('is-sidebar-open') ? '' : 'hide');
+            }
+        });
+        const sidebarToggled = localStorage.getItem('sidebar') == 'hide' ? false : true;
+        document.body.clientWidth > 959 &&
+            (document.documentElement.classList.contains('is-sidebar-open') != sidebarToggled)
+            && simulateClick(sidebarButton);
     }
-    else
-        setTimeout(processSidebarButton, 200);
+
 }
 let current_page_url = '';
 function pageChanged() {
-      const sidebarLinks = Array.from(document.querySelectorAll('.sidebar-links li'));
+    processSidebarButton();
+    const sidebarLinks = Array.from(document.querySelectorAll('.sidebar-links li'));
     for (const li of sidebarLinks) li.classList.remove('active');
-    setTimeout( function(){ 
-    const sidebarActive = document.querySelector('.sidebar-link.active');
-    sidebarActive && sidebarActive.parentElement.classList.add('active');
-    },300);
+    setTimeout(function () {
+        const sidebarActive = document.querySelector('.sidebar-link.active');
+        if (sidebarActive && sidebarActive.parentElement) {
+            sidebarActive && sidebarActive.parentElement.classList.add('active');
+            const hasChildren = !!sidebarActive.parentElement.querySelector('ul');
+            if (hasChildren) {
+                sidebarActive.parentElement.classList.add('has-children');
+                sidebarActive.parentElement.classList.remove('no-children');
+            }
+            else {
+                sidebarActive.parentElement.classList.remove('has-children');
+                sidebarActive.parentElement.classList.add('no-children');
+            }
+        }
+    }, 300);
 }
 function _pageChanged() {
-     const contentElement = document.querySelector('.content');
-     
+    const contentElement = document.querySelector('.content');
     if (contentElement && current_page_url != location.pathname) {
         current_page_url = location.pathname;
         pageChanged();
     }
 }
-setInterval(_pageChanged, 400);
-processSidebarButton();
+
+try {
+    if (window && document && document.body) {
+        setInterval(_pageChanged, 400);
+
+    }
+} catch (exc1) {
+
+}
